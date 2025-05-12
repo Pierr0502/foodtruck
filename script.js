9854e37ea8155a6adaf67c3b55181aa4fc001930
Pierr0502 <sauvegrainpml@gmail.com>
2025-05-12 17:38:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,15 @@ document.getElementById('order-form').addEventListener('submit', function(event)
     document.getElementById('commande').style.display = 'none';
     document.getElementById('confirmation').style.display = 'block';
     document.getElementById('confirmation-message').innerText = `Votre commande numéro ${orderNumber} a été passée. Vous avez commandé un(e) ${plat}.`;
-
-    emailjs.send('service_q67jhal', 'template_g66jtml')
+    
+  const emailParams = {
+        "To Email": "hermend.paularthur@gmail.com",
+        "From Name": nom,
+        order_id: orderNumber,
+        name: plat
+    };
+    
+    emailjs.send('service_q67jhal', 'template_g66jtml', emailParams)
         .then(function(response) {
             console.log('Commande envoyée avec succès', response);
         }, function(error) {
